ec1d0bae48d3db7d7f390930bcc692d1e3eccc01
Luis Conde <brotherowsky@gmail.com>
2021-04-14 14:19:23 -0500

    version 25 - withoutTests

--- src/test/java/usantatecla/movies/v25/CustomerTest.java
+++ src/test/java/usantatecla/movies/v25/CustomerTest.java
@@ -1,5 +1,6 @@
 package usantatecla.movies.v25;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -7,13 +8,41 @@ import static org.junit.Assert.assertEquals;
 
 public class CustomerTest {
 
+    String customerName = "customerName";
+    private Customer customer;
+
+    @Before
+    public void before() {
+        customer = new CustomerBuilder().name(customerName).build();
+    }
+
     @Test
-    public void withoutRentalsTest() {
-        String customerName = "customerName";
-        Customer customer = new CustomerBuilder().name(customerName).build();
-
+    public void withoutRentalsCustomerNameTest() {
         String statement = customer.statement();
+        assertEquals(customer.getName(), new StatementDecompose(statement).getCustomerName());
+    }
 
+    @Test
+    public void  withoutRentalsTotalAmountTest() {
+        String statement = customer.statement();
+        assertEquals(0, new StatementDecompose(statement).getTotalAmount(), 0.000001);
+    }
+
+    @Test
+    public void withoutRentalsMoviesTest() {
+        String statement = customer.statement();
+        assertEquals("", new StatementDecompose(statement).getMovies());
+    }
+
+    @Test
+    public void withoutRentalsFrequentRenterPointsTest() {
+        String statement = customer.statement();
+        assertEquals(0, new StatementDecompose(statement).getFrequentRenterPoints());
+    }
+
+    @Test
+    public void withoutRentalsFormatStatementTest() {
+        String statement = customer.statement();
         String result = new StatementBuilder().customerName(customerName)
                 .totalAmount(0).frequentRenterPoints(0).build();
         assertEquals(result, statement);
